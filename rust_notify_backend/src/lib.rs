5927236f892493ae0ad1441d15cc9ff7c4188122
Samuel Colvin <s@muelcolvin.com>
2022-03-15 15:29:50 +0000

    debouncing of raw events

--- rust_notify_backend/src/lib.rs
+++ rust_notify_backend/src/lib.rs
@@ -1,24 +1,28 @@
 extern crate notify;
 extern crate pyo3;
 
-use pyo3::exceptions::PyRuntimeError;
+use pyo3::exceptions::{PyRuntimeError, PyKeyboardInterrupt};
 use pyo3::prelude::*;
 use pyo3::{create_exception};
+use pyo3::ffi::PyErr_CheckSignals;
 
-use notify::{watcher, DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
+use std::collections::HashSet;
 use std::path::PathBuf;
 use std::sync::mpsc::{channel, Receiver};
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
+
+use notify::{raw_watcher, RawEvent, op, RecommendedWatcher, RecursiveMode, Watcher};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-create_exception!(_rtoml, WatchgodRustInternalError, PyRuntimeError);
+create_exception!(_rust_notify_backend, WatchgodRustInternalError, PyRuntimeError);
 
 #[pyclass]
 struct RustNotifyWatcher {
-    rx: Receiver<DebouncedEvent>,
+    rx: Receiver<RawEvent>,
     _watcher: RecommendedWatcher,
 }
 
+// these need to match `watchgod/watcher.py::Change`
 const CHANGE_ADDED: u8 = 1;
 const CHANGE_MODIFIED: u8 = 2;
 const CHANGE_DELETED: u8 = 3;
@@ -30,61 +34,107 @@ fn path_to_string(p: PathBuf) -> Option<String> {
     }
 }
 
+fn check_signals() -> bool {
+    unsafe {
+        let v = PyErr_CheckSignals();
+        if v == -1 {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+
 #[pymethods]
 impl RustNotifyWatcher {
     #[new]
-    fn py_new(watch_path: String, debounce_ms: u64) -> PyResult<Self> {
+    fn py_new(watch_path: String) -> PyResult<Self> {
         // Create a channel to receive the events.
         let (tx, rx) = channel();
 
         // The notification back-end is selected based on the platform.
-        let mut _watcher = watcher(tx, Duration::from_millis(debounce_ms)).unwrap();
+        let mut _watcher = raw_watcher(tx).unwrap();
 
         _watcher.watch(watch_path, RecursiveMode::Recursive).unwrap();
         Ok(RustNotifyWatcher { rx, _watcher })
     }
 
-    // WARNING: keep this low as the wait is not cancellable by KeyboardInterrupt or similar
-    // see https://stackoverflow.com/q/62364030/949890
-    #[args(debounce_ms = 50)]
-    pub fn check(&self, py: Python, debounce_ms: u64) -> PyResult<PyObject> {
-        match self.rx.recv_timeout(Duration::from_millis(debounce_ms)) {
-            Ok(event) => {
-                let tuple: (u8, Option<String>) = match event {
-                    DebouncedEvent::Create(path) => {
-                        (CHANGE_ADDED, path_to_string(path))
-                    }
-                    DebouncedEvent::Write(path) => {
-                        (CHANGE_ADDED, path_to_string(path))
-                    }
-                    DebouncedEvent::Chmod(path) => {
-                        (CHANGE_MODIFIED, path_to_string(path))
-                    }
-                    DebouncedEvent::Remove(path) => {
-                        (CHANGE_DELETED, path_to_string(path))
-                    }
-                    DebouncedEvent::Rename(old_path, new_path) => {
-                        // two events, manually return from there
-                        let e1 = (CHANGE_DELETED, path_to_string(old_path));
-                        let e2 = (CHANGE_ADDED, path_to_string(new_path));
-                        let v  = vec![e1, e2];
-                        return Ok(v.to_object(py));
-                    }
-                    DebouncedEvent::Error(error, _) => {
-                        return Err(WatchgodRustInternalError::new_err(format!("{:?}", error)))
-                    }
-                    // NoticeWrite, NoticeRemove, Rescan
-                    _ => {
-                        let none: Option<String> = None;
-                        return Ok(none.to_object(py));
-                    }
-                };
-                Ok(vec![tuple].to_object(py))
+    // WARNING: keep step_size low as the wait is not cancelled by KeyboardInterrupt
+    #[args(debounce_ms = 1600, step_size = 50)]
+    pub fn check(&self, py: Python, debounce_ms: u64, step_size: u64) -> PyResult<PyObject> {
+        let mut changes = Vec::<(u8, Option<String>)>::new();
+        let max_time = SystemTime::now() + Duration::from_millis(debounce_ms);
+        let recv_timeout = Duration::from_millis(step_size);
+        let mut rename_cookies = HashSet::<u32>::new();
+        loop {
+            if check_signals() {
+                return Err(PyKeyboardInterrupt::new_err(""));
             }
-            _ => {
-                // timeout
-                let none: Option<String> = None;
-                return Ok(none.to_object(py));
+            let new_changes = match self.rx.recv_timeout(recv_timeout) {
+                Ok(event) => {
+                    // println!("event: {:?}", event);
+                    match event.op {
+                        Ok(op) => {
+                            if let Some(path) = event.path {
+                                let change: Option<u8> = match op {
+                                    op::CREATE => Some(CHANGE_ADDED),
+                                    op::CHMOD | op::WRITE => Some(CHANGE_MODIFIED),
+                                    op::REMOVE => Some(CHANGE_DELETED),
+                                    op::RENAME => {
+                                        if let Some(cookie) = event.cookie {
+                                            if rename_cookies.contains(&cookie) {
+                                                Some(CHANGE_ADDED)
+                                            } else {
+                                                rename_cookies.insert(cookie);
+                                                Some(CHANGE_DELETED)
+                                            }
+                                        } else {
+                                            None
+                                        }
+                                    },
+                                    op::RESCAN => None,
+                                    _ => {
+                                        if op.contains(op::REMOVE) {
+                                            Some(CHANGE_DELETED)
+                                        } else if op.contains(op::CREATE) {
+                                            Some(CHANGE_ADDED)
+                                        } else if op == op::Op::empty() {
+                                            None
+                                        } else {
+                                            print!("event with unknown op {:?}, path={:?}", op, path);
+                                            None
+                                            // let msg = format!("event with unknown op {:?}, path={:?}", op, path);
+                                            // return Err(WatchgodRustInternalError::new_err(msg));
+                                        }
+                                    },
+                                };
+                                if let Some(change) = change {
+                                    let path = path_to_string(path);
+                                    changes.push((change, path));
+                                    true
+                                } else {
+                                    false
+                                }
+                            } else {
+                                let msg = format!("event with no path, op={:?}", op);
+                                return Err(WatchgodRustInternalError::new_err(msg));
+                            }
+                        },
+                        Err(e) => {
+                            let msg = format!("{}", e);
+                            return Err(WatchgodRustInternalError::new_err(msg));
+                        },
+                    }
+                },
+                _ => {
+                    // timeout
+                    false
+                }
+            };
+
+            if !new_changes || SystemTime::now() > max_time {
+                return Ok(changes.to_object(py));
             }
         }
     }
