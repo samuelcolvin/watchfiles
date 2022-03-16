fd22e269e05a222d060f11b4d89dfaa7af9bad24
Samuel Colvin <s@muelcolvin.com>
2022-03-16 23:21:38 +0000

    remove trio from tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,10 +40,9 @@ def tmp_work_path(tmp_path: Path):
 @pytest.fixture(
     params=[
         pytest.param(('asyncio', {'use_uvloop': True}), id='asyncio+uvloop'),
-        # pytest.param(('asyncio', {'use_uvloop': False}), id='asyncio'),
-        # pytest.param(('trio', {'restrict_keyboard_interrupt_to_checkpoints': True}), id='trio'),
+        pytest.param(('asyncio', {'use_uvloop': False}), id='asyncio'),
     ],
     autouse=True,
 )
 def anyio_backend(request):
-    return 'asyncio'
+    return request.param
