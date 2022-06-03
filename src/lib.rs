d8f26eb1f1caad5c25aa628b342441120c2b2a52
Samuel Colvin <s@muelcolvin.com>
2022-06-03 16:57:55 +0100

    more details in version comment

--- src/lib.rs
+++ src/lib.rs
@@ -242,6 +242,9 @@ fn _rust_notify(py: Python, m: &PyModule) -> PyResult<()> {
     let mut version = env!("CARGO_PKG_VERSION").to_string();
     // cargo uses "1.0-alpha1" etc. while python uses "1.0.0a1", this is not full compatibility,
     // but it's good enough for now
+    // see https://docs.rs/semver/1.0.9/semver/struct.Version.html#method.parse for rust spec
+    // see https://peps.python.org/pep-0440/ for python spec
+    // it seems the dot after "alpha/beta" e.g. "-alpha.1" is not necessary, hence why this works
     version = version.replace("-alpha", "a").replace("-beta", "b");
     m.add("__version__", version)?;
     m.add(
